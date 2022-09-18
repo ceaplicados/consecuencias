a2f173c26e225ec6b9c9c8f2f77af527b8f4f5a9
álex gonzalpi <alex@gonzalpi.com>
2022-09-17 22:07:44 -0500

    Add connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 
-require('dotenv').config();
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const dbHost = process.env.MONGO_HOST;
+const dbPort = process.env.MONGO_PORT;
+const dbUser = process.env.MONGO_USER;
+const dbPass = process.env.MONGO_PASS;
+const dbName = process.env.MONGO_NAME;
+const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+mongoose.connect(uri, {useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection successful');
+})
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
