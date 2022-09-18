b33291242f732c33b7a57de129de2605c16576b7
álex gonzalpi <alex@gonzalpi.com>
2022-09-18 00:36:58 -0500

    Add linter rules

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true,
+    },
+    'extends': [
+        'eslint:recommended',
+    ],
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'align-assignments',
+    ],
+    'rules': {
+        // force single quotes
+        'quotes': ['error', 'single'],
+        // force semicolons
+        'semi': ['error', 'always'],
+        // 4-space indentation
+        'indent': ['error', 4],
+        // align assignments
+        'align-assignments/align-assignments': [2, { 'requiresOnly': false }],
+        // consistent spacing inside braces
+        'object-curly-spacing': ['error', 'always'],
+        'array-bracket-spacing': ['error', 'never'],
+        // force trailing commas
+        'comma-dangle': ['error', 'always-multiline'],
+        'eol-last': ['error', 'always'],
+        'key-spacing': ['error', { 'afterColon': true }],
+    },
+};
