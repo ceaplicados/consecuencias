46bb2a747cf8e24591778aa976dc949d50942278
álex gonzalpi <alex@gonzalpi.com>
2022-09-18 14:09:40 -0500

    Add nodes endpoints to read, update and delete, #10, #12, #18

--- server/routes/nodes.js
+++ server/routes/nodes.js
@@ -23,4 +23,30 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Node.findById(req.params.id)
+        .then(node => res.json(node))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Node.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Node deleted!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Node.findById(req.params.id)
+        .then(node => {
+            node.title = req.body.title || node.title;
+            node.body  = req.body.body || node.body;
+            node.type  = req.body.type || node.type;
+
+            node.save()
+                .then(() => res.json('Node updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
