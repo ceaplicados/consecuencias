b376f72be72442e147dc1eeca91322775323d7cc
álex gonzalpi <alex@gonzalpi.com>
2022-09-18 00:43:07 -0500

    Make server/index.js conform to linter rules

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express  = require('express');
+const path     = require('path');
 const mongoose = require('mongoose');
 
-require('dotenv').config({path: path.resolve(__dirname, '.env')});
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-const app = express();
+const app  = express();
 const port = process.env.PORT || 3001;
 
 const dbHost = process.env.MONGO_HOST;
@@ -12,13 +12,13 @@ const dbPort = process.env.MONGO_PORT;
 const dbUser = process.env.MONGO_USER;
 const dbPass = process.env.MONGO_PASS;
 const dbName = process.env.MONGO_NAME;
-const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
-mongoose.connect(uri, {useNewUrlParser: true});
+const uri    = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+mongoose.connect(uri, { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB connection successful');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
-})
+});
